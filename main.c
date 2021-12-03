012eb1b8f1d6b17dca877fa1a673ed25efab5437
Alexandre Oliver <Alexandreferreiradeoliveira40@gmail.com>
2021-12-03 12:52:14 -0300

    Resolvendo os bugs dos tipos de dados

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ void limpar(void)
 int main( void )
 {
     float preco, valor;
-    unsigned char qtd, taxa, cont;
+    short int qtd, taxa, cont;
     char produto[20];
 
     limpar();
@@ -17,16 +17,16 @@ int main( void )
     printf("\nDigite o nome do produto: ");
     scanf("%s", &produto);
     printf("Digite o custo: ");
-    scanf("%f", &valor);  // quando for executar o programa pesta atenção no valor que vc vai colocar aqui
+    scanf("%f", &valor);
     printf("Digite a taxa(%%): ");
-    scanf("%d", &taxa);
-    printf("Variavel Custo = %f", valor); // isso aqui é para mostrar q ate aqui o programa ta correto, mostrando o valor definido por vc.
+    scanf("%hd", &taxa);
+    printf("\nVariavel Custo = %f", valor);
     printf("\nDigite a quantidade: ");
-    scanf("%d", &qtd);
+    scanf("%hd", &qtd);
     
 
-    printf("\nProduto: %s, no valor R$%.2f com a taxa %d%%, quantidade %d \n", produto, valor, taxa, qtd);
-    printf("Variavel Custo = %f\n", valor); // mas aqui ele responde um valor diferente daquele q vc colocou na variavel.
+    printf("\nProduto: %s, no valor R$%.2f com a taxa %hd%%, quantidade %hd \n", produto, valor, taxa, qtd);
+    printf("Variavel Custo = %f\n", valor);
 
     return 0;
 }
